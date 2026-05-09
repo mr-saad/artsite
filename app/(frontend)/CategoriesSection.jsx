b5fe9af9d3f5d7739cd372ae084835fe746661fb
saadkhatri1 <khatrisaad233@gmail.com>
2026-05-09 17:04:12 +0530

    featuredCategories query fixes

--- app/(frontend)/CategoriesSection.jsx
+++ app/(frontend)/CategoriesSection.jsx
@@ -3,10 +3,16 @@ import { getGlobal } from "@/lib/server/getData"
 
 export default async function CategoriesSection() {
   const gb = await getGlobal({ slug: "featuredCategories" })
+  if (!gb.categories)
+    return (
+      <p className="text-2xl text-red-600 p-5 bg-white">
+        Error: Cannot find Categories
+      </p>
+    )
 
   return (
-    gb.categories.length > 0 &&
-    gb.categories.map((ct) => {
+    gb?.categories.length > 0 &&
+    gb?.categories.map((ct) => {
       return (
         <Link
           className="block font-serif text-3xl transition p-5 outline text-white hover:text-black hover:bg-zinc-100 focus-visible:text-black focus-visible:bg-zinc-100"
--- app/(frontend)/ProductSections.jsx
+++ app/(frontend)/ProductSections.jsx
@@ -4,9 +4,15 @@ import Section from "@/components/ui/section"
 
 export default async function ProductSections() {
   const gb = await getGlobal({ slug: "featuredSections" })
+  if (!gb.sections)
+    return (
+      <p className="text-2xl text-red-600 p-5 bg-white">
+        Error: Cannot find Sections
+      </p>
+    )
   return (
-    gb.sections.length > 0 &&
-    gb.sections.map((section) => {
+    gb?.sections.length > 0 &&
+    gb?.sections.map((section) => {
       return (
         <Section key={section.category.slug}>
           <div className="text-center">
